5dc378b3258a75020e8ff69a4d56bbf17584827b
Mateo Deget <degetmateo@gmail.com>
2023-10-24 18:48:07 -0300

    urls api

---
+++ apis.js
@@ -0,0 +1,3 @@
+const urlMercadoLibre = 'https://api.mercadolibre.com/sites/MLA/search?q=';
+const urlCotizador = 'https://www.dolarsi.com/api/api.php?type=' //dolar, cotizador, valoresprincipales
+const urlBlue = 'https://api.bluelytics.com.ar/v2/latest';
