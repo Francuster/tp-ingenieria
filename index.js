b0d9948b0640d0363f35c4688f45e2b1c4a01546
Francuster <francuster1@gmail.com>
2023-10-24 19:14:52 -0300

    fix map

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-const coords = [-34.5129782, -58.6869033];
+var map = L.map('map').setView([51.505, -0.09], 13);
 
-function crearMapa() {
-    var map = L.map('map').setView(coords, 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    }).addTo(map);
-
-    var marker = L.marker(coords).addTo(map);
-}
-
-crearMapa();
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    .openPopup();
