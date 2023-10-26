fc1915c696f47a7690e095dd2f9e784b39d2a52e
Mateo Deget <degetmateo@gmail.com>
2023-10-26 16:38:46 -0300

    busqueda basica productos

--- js/apis.js
+++ js/apis.js
@@ -1,3 +1,40 @@
 const urlMercadoLibre = 'https://api.mercadolibre.com/sites/MLA/search?q=';
 const urlCotizador = 'https://www.dolarsi.com/api/api.php?type=' //dolar, cotizador, valoresprincipales
 const urlBlue = 'https://api.bluelytics.com.ar/v2/latest';
+
+
+const contenedorProductos = document.getElementById('contenedor-productos');
+const inputBuscarProductos = document.getElementById('input-buscar-productos');
+
+inputBuscarProductos.addEventListener('keydown', (event) => {
+    if (event.key.toLowerCase() === 'enter') {
+        const busqueda = inputBuscarProductos.value;
+        buscarProductos(busqueda)
+    }
+})
+
+async function buscarProductos (busqueda) {
+    const peticion = await fetch(urlMercadoLibre + busqueda, { method: 'GET' })
+    const respuesta = await peticion.json();
+    const resultados = respuesta.results;
+    console.log(resultados)
+    pintarProductos(resultados);
+}
+
+function pintarProductos (resultados) {
+    contenedorProductos.innerHTML = '';
+
+    for (const resultado of resultados) {
+        contenedorProductos.innerHTML += `
+            <div class='card'>
+                <div class='card-body'>
+                    <img src="${resultado.thumbnail}" class="card-img-top" alt="Producto">
+                    <h6 class='card-title'>${resultado.title}</h6>
+                    <p class='card-text'>Precio: $${resultado.price}</p>
+                    <p class='card-text'>Vendedor: ${resultado.seller.nickname}</p>
+                    <a href="#" class="btn btn-primary">Comprar</a>
+                </div>
+            </div>
+        `
+    }
+}
