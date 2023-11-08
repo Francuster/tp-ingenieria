19eddc8971983cbbfea9d59d4ee9c0fc4ca80a8a
Franco Disabato <francuster1@gmail.com>
2023-11-08 10:11:53 -0300

    Create sitios-interes.js

---
+++ js/sitios-interes.js
@@ -0,0 +1,13 @@
+//hardcodear sitios de interes y atractivos turisticos
+
+const arraySitiosInteres = [
+    {nombre: 'UNGS', descripcion: 'Universidad Nacional de General Sarmiento', foto:'https://www.ungs.edu.ar/wp-content/uploads/2019/08/Campus-aérea4.png', coordenadas: [-34.5129782, -58.6869033]},
+    {nombre: 'UNGS', descripcion: 'Universidad Nacional de General Sarmiento', foto:'https://www.ungs.edu.ar/wp-content/uploads/2019/08/Campus-aérea4.png', coordenadas: [-34.5129782, -58.6869033]}
+
+]
+
+const arrayAtractivos = [
+    {resumen: 'Biblioteca', descripcionCompleta: 'Amplia biblioteca publica disponible de 9 a 18 hs', foto: 'https://www.ungs.edu.ar/wp-content/uploads/2022/06/biblioteca-horacio-gonzalez-ungs.jpg', nombreSitio: 'UNGS'},
+    {resumen: 'Buffet', descripcionCompleta: 'Lugar para comer', foto: 'https://www.ungs.edu.ar/wp-content/uploads/2022/06/biblioteca-horacio-gonzalez-ungs.jpg', nombreSitio: 'UNGS'}
+
+]
