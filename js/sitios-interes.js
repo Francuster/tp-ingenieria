a4a9976cad837820a43443eaff67638ccd3c950c
Santiago Agustín Gomez Riaño <gomezsantiago879@gmail.com>
2023-11-11 23:07:12 -0300

    Sitios de interés adicionales

--- js/sitios-interes.js
+++ js/sitios-interes.js
@@ -11,21 +11,83 @@ const arraySitiosInteres = [
                 resumen: 'Biblioteca',
                 descripcionCompleta: 'Amplia biblioteca publica disponible de 9 a 18 hs',
                 foto: 'https://www.ungs.edu.ar/wp-content/uploads/2022/06/biblioteca-horacio-gonzalez-ungs.jpg',
-                nombreSitio: 'UNGS'
             },
             {
                 resumen: 'Buffet',
                 descripcionCompleta: 'Lugar para comer',
                 foto: 'https://www.ungs.edu.ar/wp-content/uploads/2022/06/biblioteca-horacio-gonzalez-ungs.jpg',
-                nombreSitio: 'UNGS'
             }
         ]
     },
     {
+        nombre: 'Municipio de Malvinas Argentinas',
+        descripcion: 'Oficina de Gobierno Local',
+        foto:'https://observatorio.madretierra.org.ar/wp-content/uploads/2022/09/municipalidad-malvinas-argentinas-696x386-1.jpg', 
+        coordenadas: [-34.49767380192895, -58.70090154542923]
+    },
+    {
         nombre: 'Terrazas de Mayo',
-        descripcion: 'Shoping Terrazas de Mayo',
+        descripcion: 'Shopping Terrazas de Mayo',
         foto:'https://www.ungs.edu.ar/wp-content/uploads/2019/08/Campus-aérea4.png', 
-        coordenadas: [-34.53012397898645, -58.701013326644905]
+        coordenadas: [-34.5484913018673, -58.71923915924889]
+    },
+    {
+        nombre: 'UNPAZ',
+        descripcion: 'Universidad Nacional de José C. Paz',
+        foto:'https://www.ungs.edu.ar/wp-content/uploads/2019/08/Campus-aérea4.png', 
+        coordenadas: [-34.520273059060116, -58.745784501275345]
+    },
+    {
+        nombre: 'Feria Persa',
+        descripcion: 'Lugar de entrenimiento asegurado',
+        foto:'https://www.ungs.edu.ar/wp-content/uploads/2019/08/Campus-aérea4.png', 
+        coordenadas: [-34.5484913018673, -58.71923915924889]
+    },
+    {
+        nombre: 'Sumeru',
+        descripcion: 'Ciudad de la Sabiduría',
+        foto:'https://static.wikia.nocookie.net/gen-impact/images/9/9d/Localizaci%C3%B3n_Ciudad_de_Sumeru.png/revision/latest/scale-to-width-down/350?cb=20221010172343&path-prefix=es', 
+        coordenadas: [-34.52685358890572, -58.67574691772462],
+        atractivos: [
+            {
+                resumen: 'Academia de Sabios',
+                descripcionCompleta: 'Amplia biblioteca publica disponible de 9 a 18 hs',
+                foto: 'https://cdn2.unrealengine.com/image-3-3840x2160-66f95019dcca.jpg',
+            },
+            {
+                resumen: 'Taberna Yafar',
+                descripcionCompleta: 'Conocido por sus extravagentes platos gastronómicos',
+                foto: 'https://static.wikia.nocookie.net/gen-impact/images/2/21/Localizaci%C3%B3n_Taberna_Yafar.png/revision/latest/scale-to-width-down/180?cb=20221014141225&path-prefix=es',
+            },
+            {
+                resumen: 'Piramide Invertida del Rey Deshret',
+                descripcionCompleta: 'Monumento principal del Desierto ',
+                foto: 'https://cdn2.unrealengine.com/01-3840x2160-daa2547ad5b8.jpg',
+            }
+        ]
+    },
+    {
+        nombre: 'Fontaine',
+        descripcion: 'Ciudad de la Justicia',
+        foto:'https://live.staticflickr.com/65535/53084287801_1e0e7a718d_h.jpg', 
+        coordenadas: [-34.56838241670848, -58.67574691772462],
+        atractivos: [
+            {
+                resumen: 'Corte de Fontaine',
+                descripcionCompleta: 'Amplia biblioteca publica disponible de 9 a 18 hs',
+                foto: 'https://cdn2.unrealengine.com/image-3-3840x2160-66f95019dcca.jpg',
+            },
+            {
+                resumen: 'Taberna Yafar',
+                descripcionCompleta: 'Conocido por sus extravagentes platos gastronómicos',
+                foto: 'https://static.wikia.nocookie.net/gen-impact/images/2/21/Localizaci%C3%B3n_Taberna_Yafar.png/revision/latest/scale-to-width-down/180?cb=20221014141225&path-prefix=es',
+            },
+            {
+                resumen: 'Piramide Invertida del Rey Deshret',
+                descripcionCompleta: 'Monumento principal del Desierto ',
+                foto: 'https://cdn2.unrealengine.com/01-3840x2160-daa2547ad5b8.jpg',
+            }
+        ]
     }
 
 ]
